a2e58bb4faac2e37da3be128092cf1018fc9c4ed
Alexander Zaytsev <thebestzorro@yandex.ru>
2015-12-25 19:20:43 +0300

    minor changes

--- conf/conf.go
+++ conf/conf.go
@@ -6,189 +6,187 @@
 package conf
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "path/filepath"
-    "strings"
-    "time"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"strings"
+	"time"
 
-    "github.com/z0rr0/hashq"
-    "github.com/z0rr0/luss/lru"
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
+	"github.com/z0rr0/luss/lru"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const (
-    // SaltsLen in minimal recommended salt length.
-    SaltsLen = 16
-    // AnonName is name of anonymous user.
-    AnonName = "anonymous"
-    // DefaultProject is name of default project.
-    DefaultProject = "system"
+	// SaltsLen in minimal recommended salt length.
+	SaltsLen = 16
+	// AnonName is name of anonymous user.
+	AnonName = "anonymous"
+	// DefaultProject is name of default project.
+	DefaultProject = "system"
 )
 
 var (
-    // AnonUser is anonymous user
-    AnonUser = User{Name: AnonName, Role: "user", Created: time.Now().UTC()}
-    // AnonProject is system project for administrative and anonymous users.
-    AnonProject = Project{Name: DefaultProject, Users: []User{AnonUser}}
+	// AnonUser is anonymous user
+	AnonUser = User{Name: AnonName, Role: "user", Created: time.Now().UTC()}
+	// AnonProject is system project for administrative and anonymous users.
+	AnonProject = Project{Name: DefaultProject, Users: []User{AnonUser}}
 )
 
 // User is structure of user's info.
 type User struct {
-    Name    string    `bson:"name"`
-    Key     string    `bson:"key"`
-    Role    string    `bson:"role"`
-    Created time.Time `bson:"ts"`
-    Secret  string    `bson:",omitempty"`
+	Name    string    `bson:"name"`
+	Key     string    `bson:"key"`
+	Role    string    `bson:"role"`
+	Created time.Time `bson:"ts"`
+	Secret  string    `bson:",omitempty"`
 }
 
 // Project is structure of project's info.
 type Project struct {
-    ID       bson.ObjectId `bson:"_id"`
-    Name     string        `bson:"name"`
-    Users    []User        `bson:"users"`
-    Modified time.Time     `bson:"modified"`
+	ID       bson.ObjectId `bson:"_id"`
+	Name     string        `bson:"name"`
+	Users    []User        `bson:"users"`
+	Modified time.Time     `bson:"modified"`
 }
 
 // security contains main security settings.
 type security struct {
-    Salt     string `json:"salt"`
-    TokenLen int    `json:"tokenlen"`
+	Salt     string `json:"salt"`
+	TokenLen int    `json:"tokenlen"`
 }
 
 // listener is HTTP server configuration
 type listener struct {
-    Host     string   `json:"host"`
-    Port     uint     `json:"port"`
-    Timeout  int64    `json:"timeout"`
-    Security security `json:"security"`
+	Host     string   `json:"host"`
+	Port     uint     `json:"port"`
+	Timeout  int64    `json:"timeout"`
+	Security security `json:"security"`
 }
 
 // MongoCfg is database configuration settings
 type MongoCfg struct {
-    Hosts       []string `json:"hosts"`
-    Port        uint     `json:"port"`
-    Timeout     uint     `json:"timeout"`
-    Username    string   `json:"username"`
-    Password    string   `json:"password"`
-    Database    string   `json:"database"`
-    AuthDB      string   `json:"authdb"`
-    ReplicaSet  string   `json:"replica"`
-    Ssl         bool     `json:"ssl"`
-    SslKeyFile  string   `json:"sslkeyfile"`
-    PrimaryRead bool     `json:"primaryread"`
-    Reconnects  int      `json:"reconnects"`
-    RcnTime     int64    `json:"rcntime"`
-    Debug       bool     `json:"debug"`
-    RcnDelay    time.Duration
-    ConChan     chan hashq.Shared
-    MongoCred   *mgo.DialInfo
-    Logger      *log.Logger
+	Hosts       []string `json:"hosts"`
+	Port        uint     `json:"port"`
+	Timeout     uint     `json:"timeout"`
+	Username    string   `json:"username"`
+	Password    string   `json:"password"`
+	Database    string   `json:"database"`
+	AuthDB      string   `json:"authdb"`
+	ReplicaSet  string   `json:"replica"`
+	Ssl         bool     `json:"ssl"`
+	SslKeyFile  string   `json:"sslkeyfile"`
+	PrimaryRead bool     `json:"primaryread"`
+	Reconnects  int      `json:"reconnects"`
+	RcnTime     int64    `json:"rcntime"`
+	Debug       bool     `json:"debug"`
+	RcnDelay    time.Duration
+	ConChan     chan hashq.Shared
+	MongoCred   *mgo.DialInfo
+	Logger      *log.Logger
 }
 
 // cacheCfg is database connections pool settings
 type cacheCfg struct {
-    DbPoolSize int   `json:"dbpoolsize"`
-    DbPoolTTL  int64 `json:"dbpoolttl"`
-    LRUSize    int   `json:"lrusize"`
-    Debug      bool  `json:"debug"`
-    LRU        *lru.Cache
+	DbPoolSize int   `json:"dbpoolsize"`
+	DbPoolTTL  int64 `json:"dbpoolttl"`
+	LRUSize    int   `json:"lrusize"`
+	Debug      bool  `json:"debug"`
+	LRU        *lru.Cache
 }
 
 // workers is main workers settings
 type workers struct {
-    CleanMin int64 `json:"cleanup"`
-    NumStats int   `json:"numstats"`
-    BufStats int   `json:"bufstat"`
-    NumCb    int   `json:"numcb"`
-    BufCb    int   `json:"bufcb"`
-    CleanD   time.Duration
-    ChStats  chan string
-    ChCb     chan string
+	CleanMin int64 `json:"cleanup"`
+	NumStats int   `json:"numstats"`
+	BufStats int   `json:"bufstat"`
+	NumCb    int   `json:"numcb"`
+	BufCb    int   `json:"bufcb"`
+	CleanD   time.Duration
+	ChStats  chan string
+	ChCb     chan string
 }
 
 // projects is projects' settings.
 type projects struct {
-    MaxSpam   int  `json:"maxspam"`
-    CbLength  int  `json:"cblength"`
-    CbAllow   bool `json:"cballow"`
-    MaxName   int  `json:"maxname"`
-    Anonymous bool `json:"anonymous"`
-    MaxPack   int  `json:"maxpack"`
+	MaxSpam   int  `json:"maxspam"`
+	CbLength  int  `json:"cblength"`
+	CbAllow   bool `json:"cballow"`
+	MaxName   int  `json:"maxname"`
+	Anonymous bool `json:"anonymous"`
+	MaxPack   int  `json:"maxpack"`
 }
 
 // domain is settings if main service domain.
 type domain struct {
-    Name    string `json:"name"`
-    Secure  bool   `json:"secure"`
-    Address string
+	Name    string `json:"name"`
+	Secure  bool   `json:"secure"`
+	Address string
 }
 
 // Config is main configuration storage.
 type Config struct {
-    Domain   domain   `json:"domain"`
-    Listener listener `json:"listener"`
-    Db       MongoCfg `json:"database"`
-    Cache    cacheCfg `json:"cache"`
-    Workers  workers  `json:"workers"`
-    Projects projects `json:"projects"`
-    Pool     *hashq.HashQ
-    Clean    chan string
+	Domain   domain   `json:"domain"`
+	Listener listener `json:"listener"`
+	Db       MongoCfg `json:"database"`
+	Cache    cacheCfg `json:"cache"`
+	Workers  workers  `json:"workers"`
+	Projects projects `json:"projects"`
+	Pool     *hashq.HashQ
+	Clean    chan string
 }
 
 // ConnCap returns a recommended connections capacity.
 func (c *Config) ConnCap() int {
-    var result int
-    switch {
-    case c.Cache.DbPoolSize > 128:
-        result = 32
-    case c.Cache.DbPoolSize > 32:
-        result = 16
-    case c.Cache.DbPoolSize > 8:
-        result = 8
-    default:
-        result = c.Cache.DbPoolSize
-    }
-    return result
+	var result int
+	switch {
+	case c.Cache.DbPoolSize > 128:
+		result = 32
+	case c.Cache.DbPoolSize > 32:
+		result = 16
+	case c.Cache.DbPoolSize > 8:
+		result = 8
+	default:
+		result = c.Cache.DbPoolSize
+	}
+	return result
 }
 
 // Address returns a full short url address.
 func (c *Config) Address(url string) string {
-    if c.Domain.Secure {
-        return fmt.Sprintf("https://%s/%s", c.Domain.Name, url)
-    }
-    return fmt.Sprintf("http://%s/%s", c.Domain.Name, url)
+	if c.Domain.Secure {
+		return fmt.Sprintf("https://%s/%s", c.Domain.Name, url)
+	}
+	return fmt.Sprintf("http://%s/%s", c.Domain.Name, url)
 }
 
 // GoodSalts verifies salts values.
 func (c *Config) GoodSalts() bool {
-    return len(c.Listener.Security.Salt) >= SaltsLen
+	return len(c.Listener.Security.Salt) >= SaltsLen
 }
 
 // Addrs return an array of available MongoDB connections addresses.
 func (cfg *MongoCfg) Addrs() []string {
-    hosts := make([]string, len(cfg.Hosts))
-    for i, host := range cfg.Hosts {
-        hosts[i] = fmt.Sprintf("%v:%v", host, cfg.Port)
-    }
-    return hosts
+	hosts := make([]string, len(cfg.Hosts))
+	for i, host := range cfg.Hosts {
+		hosts[i] = fmt.Sprintf("%v:%v", host, cfg.Port)
+	}
+	return hosts
 }
 
 // ParseConfig reads a configuration file and
 // returns a pointer to prepared Config structure.
 func ParseConfig(name string) (*Config, error) {
-    cfg := &Config{}
-    fullpath, err := filepath.Abs(strings.Trim(name, " "))
-    if err != nil {
-        return cfg, err
-    }
-    jsondata, err := ioutil.ReadFile(fullpath)
-    if err != nil {
-        return cfg, err
-    }
-    err = json.Unmarshal(jsondata, cfg)
-    return cfg, err
+	cfg := &Config{}
+	fullpath, err := filepath.Abs(strings.Trim(name, " "))
+	if err != nil {
+		return cfg, err
+	}
+	jsondata, err := ioutil.ReadFile(fullpath)
+	if err != nil {
+		return cfg, err
+	}
+	err = json.Unmarshal(jsondata, cfg)
+	return cfg, err
 }
--- db/db.go
+++ db/db.go
@@ -6,281 +6,339 @@
 package db
 
 import (
-    "crypto/tls"
-    "crypto/x509"
-    "errors"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net"
-    "os"
-    "sync"
-    "time"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
 
-    "github.com/z0rr0/hashq"
-    "github.com/z0rr0/luss/conf"
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
+	"github.com/z0rr0/hashq"
+	"github.com/z0rr0/luss/conf"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const (
-    // maxLockAttempts is a number attempts to lock a collection
-    // before an error will be returned.
-    maxLockAttempts = 7 // >= 127 ms
+	// maxLockAttempts is a number attempts to lock a collection
+	// before an error will be returned.
+	maxLockAttempts = 7 // >= 127 ms
 )
 
 var (
-    // Logger is a logger for error messages
-    Logger = log.New(os.Stderr, "LOGGER [luss/db]: ", log.Ldate|log.Ltime|log.Lshortfile)
-    // Colls is a map of db collections.
-    Colls = map[string]string{
-        "test":     "test",
-        "users":    "users",
-        "urls":     "urls",
-        "locks":    "locks",
-        "ustats":   "ustats",
-        "projects": "projects",
-    }
-    // ErrDbDuplicate is error of db duplicate error.
-    ErrDbDuplicate = errors.New("db duplicate item")
+	// Logger is a logger for error messages
+	Logger = log.New(os.Stderr, "LOGGER [db]: ", log.Ldate|log.Ltime|log.Lshortfile)
+	// Colls is a map of db collections.
+	Colls = map[string]string{
+		"test":     "test",
+		"users":    "users",
+		"urls":     "urls",
+		"locks":    "locks",
+		"ustats":   "ustats",
+		"projects": "projects",
+	}
 )
+
+// Item is any DB item, it contains only identifier.
+type Item struct {
+	ID bson.ObjectId `bson:"_id"`
+}
+
+// Conn is database connection structure.
+type Conn struct {
+	S   *mgo.Session
+	m   sync.Mutex
+	Cfg *conf.MongoCfg
+}
+
+func (c *Conn) connect() error {
+	c.m.Lock()
+	defer c.m.Unlock()
+	if c.S == nil {
+		s, err := mongoDBConnection(c.Cfg)
+		if err != nil {
+			return err
+		}
+		c.S = s
+	} else if c.S.Ping() != nil {
+		s, err := mongoDBConnection(c.Cfg)
+		if err != nil {
+			return err
+		}
+		oldS := c.S
+		c.S = s
+		oldS.Close()
+	}
+	return nil
+}
+
+func (c *Conn) GetConn(secondary bool) (*mgo.Session, error) {
+	if c.S.Ping() != nil {
+		err := c.connect()
+		if err != nil {
+			return nil, err
+		}
+	}
+	s := c.S.Copy()
+	if secondary {
+		s.SetMode(mgo.SecondaryPreferred, true)
+	}
+	return s, nil
+}
+
+// New initializes main database connection and returns it.
+func New(cfg *conf.MongoCfg) (*Conn, error) {
+	conn := &Conn{Cfg: cfg}
+	err := conn.connect()
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func C(s *mgo.Session, name string) *mgo.Collection {
+	return s.DB("").C(name)
+}
 
 // Conn is database connection structure.
 // mgo is thread-safe and pings a connection after its opening.
 type Conn struct {
-    Session *mgo.Session
-    mutex   sync.RWMutex
-    one     sync.Once
-    rlu     bool // recently used
+	Session *mgo.Session
+	mutex   sync.RWMutex
+	one     sync.Once
+	rlu     bool // recently used
 }
 
 // New creates new Conn structure.
 func (c *Conn) New() hashq.Shared {
-    return &Conn{}
+	return &Conn{}
 }
 
 // Close closes an opened connection. d is a timeout after closing.
 // Connection pool can't be decreased, so it is not blocked in this method.
 func (c *Conn) Close(d time.Duration) bool {
-    c.mutex.Lock()
-    defer c.mutex.Unlock()
-    if c.Session == nil {
-        c.one = sync.Once{}
-        return false
-    }
-    // don't close a connection immediately,
-    // only if it was not used during all d period.
-    if c.rlu {
-        c.rlu = false
-        return false
-    }
-    c.Session.Close()
-    c.Session, c.one = nil, sync.Once{}
-    time.Sleep(d)
-    return true
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	if c.Session == nil {
+		c.one = sync.Once{}
+		return false
+	}
+	// don't close a connection immediately,
+	// only if it was not used during all d period.
+	if c.rlu {
+		c.rlu = false
+		return false
+	}
+	c.Session.Close()
+	c.Session, c.one = nil, sync.Once{}
+	time.Sleep(d)
+	return true
 }
 
 // Open opens new database connection but only if it wasn't ready before.
 // The boolean returned value will be true, it a connection was opened firstly.
 func (c *Conn) Open(cfg *conf.MongoCfg) (bool, error) {
-    var (
-        err   error
-        isNew bool
-    )
-    c.lock()
-    openOnce := func() {
-        s, serr := MongoDBConnection(cfg)
-        if serr != nil {
-            err = serr
-            return
-        }
-        c.Session, isNew = s, true
-    }
-    c.one.Do(openOnce)
-    return isNew, err
+	var (
+		err   error
+		isNew bool
+	)
+	c.lock()
+	openOnce := func() {
+		s, serr := MongoDBConnection(cfg)
+		if serr != nil {
+			err = serr
+			return
+		}
+		c.Session, isNew = s, true
+	}
+	c.one.Do(openOnce)
+	return isNew, err
 }
 
 func (c *Conn) lock() {
-    // Logger.Printf("call lock for %p", c)
-    c.mutex.RLock()
-    // Logger.Printf("locked  %p", c)
+	// Logger.Printf("call lock for %p", c)
+	c.mutex.RLock()
+	// Logger.Printf("locked  %p", c)
 }
 
 // release releases connection resource, but doesn't close.
 func (c *Conn) release() {
-    // Logger.Printf("call unlock for %p", c)
-    c.mutex.RUnlock()
+	// Logger.Printf("call unlock for %p", c)
+	c.mutex.RUnlock()
 }
 
 // C returns a pointer to the database collection cname.
 // It is only a wrapper method, and should be called only if connection c is opened.
 func (c *Conn) C(cname string) *mgo.Collection {
-    // default database from mgo.DialInfo will be used.
-    return c.Session.DB("").C(cname)
+	// default database from mgo.DialInfo will be used.
+	return c.Session.DB("").C(cname)
 }
 
 // MongoCredential initializes MongoDB credentials.
 func MongoCredential(cfg *conf.MongoCfg) error {
-    if cfg.Ssl {
-        pool := x509.NewCertPool()
-        pemData, err := ioutil.ReadFile(cfg.SslKeyFile)
-        if err != nil {
-            return err
-        }
-        ok := pool.AppendCertsFromPEM(pemData)
-        if !ok {
-            return errors.New("invalid certificate")
-        }
-        cert, err := tls.X509KeyPair(pemData, pemData)
-        if err != nil {
-            return err
-        }
-        tlsConfig := &tls.Config{
-            RootCAs:      pool,
-            Certificates: []tls.Certificate{cert},
-        }
-        dial := func(addr *mgo.ServerAddr) (net.Conn, error) {
-            conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
-            if err != nil {
-                cfg.Logger.Printf("tls.Dial(%s) failed with %v", addr, err)
-                return nil, err
-            }
-            cfg.Logger.Printf("SSL connection: %v", addr.String())
-            return conn, nil
-        }
-        cfg.MongoCred = &mgo.DialInfo{
-            Addrs:          cfg.Addrs(),
-            Timeout:        time.Duration(cfg.Timeout) * time.Second,
-            Database:       cfg.Database,
-            Source:         cfg.AuthDB,
-            Username:       cfg.Username,
-            Password:       cfg.Password,
-            ReplicaSetName: cfg.ReplicaSet,
-            DialServer:     dial,
-        }
-    } else {
-        cfg.MongoCred = &mgo.DialInfo{
-            Addrs:          cfg.Addrs(),
-            Timeout:        time.Duration(cfg.Timeout) * time.Second,
-            Database:       cfg.Database,
-            Source:         cfg.AuthDB,
-            Username:       cfg.Username,
-            Password:       cfg.Password,
-            ReplicaSetName: cfg.ReplicaSet,
-        }
-    }
-    return nil
+	if cfg.Ssl {
+		pool := x509.NewCertPool()
+		pemData, err := ioutil.ReadFile(cfg.SslKeyFile)
+		if err != nil {
+			return err
+		}
+		ok := pool.AppendCertsFromPEM(pemData)
+		if !ok {
+			return errors.New("invalid certificate")
+		}
+		cert, err := tls.X509KeyPair(pemData, pemData)
+		if err != nil {
+			return err
+		}
+		tlsConfig := &tls.Config{
+			RootCAs:      pool,
+			Certificates: []tls.Certificate{cert},
+		}
+		dial := func(addr *mgo.ServerAddr) (net.Conn, error) {
+			conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
+			if err != nil {
+				cfg.Logger.Printf("tls.Dial(%s) failed with %v", addr, err)
+				return nil, err
+			}
+			cfg.Logger.Printf("SSL connection: %v", addr.String())
+			return conn, nil
+		}
+		cfg.MongoCred = &mgo.DialInfo{
+			Addrs:          cfg.Addrs(),
+			Timeout:        time.Duration(cfg.Timeout) * time.Second,
+			Database:       cfg.Database,
+			Source:         cfg.AuthDB,
+			Username:       cfg.Username,
+			Password:       cfg.Password,
+			ReplicaSetName: cfg.ReplicaSet,
+			DialServer:     dial,
+		}
+	} else {
+		cfg.MongoCred = &mgo.DialInfo{
+			Addrs:          cfg.Addrs(),
+			Timeout:        time.Duration(cfg.Timeout) * time.Second,
+			Database:       cfg.Database,
+			Source:         cfg.AuthDB,
+			Username:       cfg.Username,
+			Password:       cfg.Password,
+			ReplicaSetName: cfg.ReplicaSet,
+		}
+	}
+	return nil
 }
 
 // MongoDBConnection is an initialization of MongoDb connection.
 func MongoDBConnection(cfg *conf.MongoCfg) (*mgo.Session, error) {
-    if cfg.MongoCred == nil {
-        err := MongoCredential(cfg)
-        if err != nil {
-            return nil, err
-        }
-    }
-    session, err := mgo.DialWithInfo(cfg.MongoCred)
-    if err != nil {
-        return nil, err
-    }
-    mode := mgo.PrimaryPreferred
-    if !cfg.PrimaryRead {
-        mode = mgo.SecondaryPreferred
-    }
-    session.SetMode(mode, true)
-    // session.EnsureSafe(&mgo.Safe{W: 1})
-    if cfg.Debug {
-        mgo.SetLogger(cfg.Logger)
-        mgo.SetDebug(true)
-    }
-    return session, nil
+	if cfg.MongoCred == nil {
+		err := MongoCredential(cfg)
+		if err != nil {
+			return nil, err
+		}
+	}
+	session, err := mgo.DialWithInfo(cfg.MongoCred)
+	if err != nil {
+		return nil, err
+	}
+	mode := mgo.PrimaryPreferred
+	if !cfg.PrimaryRead {
+		mode = mgo.SecondaryPreferred
+	}
+	session.SetMode(mode, true)
+	// session.EnsureSafe(&mgo.Safe{W: 1})
+	if cfg.Debug {
+		mgo.SetLogger(cfg.Logger)
+		mgo.SetDebug(true)
+	}
+	return session, nil
 }
 
 // ReleaseConn unlocks a connection if it exists.
 func ReleaseConn(c *Conn) {
-    if c != nil {
-        c.release()
-    }
+	if c != nil {
+		c.release()
+	}
 }
 
 // GetConn returns a database connection from the pool.
 // Caller should run conn.Release() after connection usage.
 func GetConn(cfg *conf.Config) (*Conn, error) {
-    isNew, err := true, errors.New("connection initial error")
-    shared := <-cfg.Db.ConChan
-    conn := shared.(*Conn)
-    for i := 0; i < cfg.Db.Reconnects; i++ {
-        isNew, err = conn.Open(&cfg.Db)
-        if !isNew && (err == nil) {
-            err = conn.Session.Ping()
-        }
-        if err == nil {
-            // Close() checks and updates this field also
-            conn.rlu = true
-            // conn is RLocked by Open()
-            return conn, nil
-        }
-        Logger.Printf("WARNING: failed %v attempt to open connection %p", i+1, conn)
-        conn.release()
-        conn.Close(cfg.Db.RcnDelay)
-    }
-    // connection is already closed
-    // don't return it to use in ReleaseConn
-    return nil, err
+	isNew, err := true, errors.New("connection initial error")
+	shared := <-cfg.Db.ConChan
+	conn := shared.(*Conn)
+	for i := 0; i < cfg.Db.Reconnects; i++ {
+		isNew, err = conn.Open(&cfg.Db)
+		if !isNew && (err == nil) {
+			err = conn.Session.Ping()
+		}
+		if err == nil {
+			// Close() checks and updates this field also
+			conn.rlu = true
+			// conn is RLocked by Open()
+			return conn, nil
+		}
+		Logger.Printf("WARNING: failed %v attempt to open connection %p", i+1, conn)
+		conn.release()
+		conn.Close(cfg.Db.RcnDelay)
+	}
+	// connection is already closed
+	// don't return it to use in ReleaseConn
+	return nil, err
 }
 
 // NewConnPool initializes new connections pool.
 func NewConnPool(cfg *conf.Config) (*hashq.HashQ, error) {
-    // TODO: should we have 2 pools for primary/secondary read prefer?
-    if cfg == nil {
-        return nil, errors.New("invalid parameter")
-    }
-    pool := hashq.New(cfg.Cache.DbPoolSize, &Conn{}, 0)
-    ch, errch := make(chan hashq.Shared, cfg.ConnCap()), make(chan error)
-    go pool.Produce(ch, errch)
-    err := <-errch
-    if err != nil {
-        return nil, err
-    }
-    go pool.Monitor(time.Duration(cfg.Cache.DbPoolTTL) * time.Second)
-    cfg.Db.ConChan = ch
-    return pool, nil
+	// TODO: should we have 2 pools for primary/secondary read prefer?
+	if cfg == nil {
+		return nil, errors.New("invalid parameter")
+	}
+	pool := hashq.New(cfg.Cache.DbPoolSize, &Conn{}, 0)
+	ch, errch := make(chan hashq.Shared, cfg.ConnCap()), make(chan error)
+	go pool.Produce(ch, errch)
+	err := <-errch
+	if err != nil {
+		return nil, err
+	}
+	go pool.Monitor(time.Duration(cfg.Cache.DbPoolTTL) * time.Second)
+	cfg.Db.ConChan = ch
+	return pool, nil
 }
 
 // CleanCollection removes all data from db collection.
 func CleanCollection(c *conf.Config, names ...string) error {
-    conn, err := GetConn(c)
-    defer ReleaseConn(conn)
-    if err != nil {
-        return err
-    }
-    for _, name := range names {
-        if err == nil {
-            coll := conn.C(name)
-            _, err = coll.RemoveAll(nil)
-        }
-    }
-    return err
+	conn, err := GetConn(c)
+	defer ReleaseConn(conn)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		if err == nil {
+			coll := conn.C(name)
+			_, err = coll.RemoveAll(nil)
+		}
+	}
+	return err
 }
 
 // LockColls adds a lock recored to name-collection
 func LockColls(name string, conn *Conn) error {
-    delay := time.Duration(time.Millisecond)
-    coll := conn.C(Colls["locks"])
-    for i := 0; i < maxLockAttempts; i++ {
-        _, err := coll.Upsert(bson.M{"_id": name, "locked": false}, bson.M{"_id": name, "locked": true})
-        if err == nil {
-            return nil
-        }
-        time.Sleep(delay)
-        delay *= 2
-    }
-    return fmt.Errorf("can't lock/update collection \"%v\" during %v attempts", Colls["locks"], maxLockAttempts)
+	delay := time.Duration(time.Millisecond)
+	coll := conn.C(Colls["locks"])
+	for i := 0; i < maxLockAttempts; i++ {
+		_, err := coll.Upsert(bson.M{"_id": name, "locked": false}, bson.M{"_id": name, "locked": true})
+		if err == nil {
+			return nil
+		}
+		time.Sleep(delay)
+		delay *= 2
+	}
+	return fmt.Errorf("can't lock/update collection \"%v\" during %v attempts", Colls["locks"], maxLockAttempts)
 }
 
 // UnlockColls removes a lock recored from name-collection.
 func UnlockColls(name string, conn *Conn) error {
-    coll := conn.C(Colls["locks"])
-    return coll.Update(bson.M{"_id": name}, bson.M{"$set": bson.M{"locked": false}})
+	coll := conn.C(Colls["locks"])
+	return coll.Update(bson.M{"_id": name}, bson.M{"$set": bson.M{"locked": false}})
 }
