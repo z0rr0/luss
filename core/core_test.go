73bb9329971c7d77499232bba0eabc63b0bf6789
Alexander Zaytsev <thebestzorro@yandex.ru>
2016-01-17 00:45:53 +0300

    core test

--- core/core_test.go
+++ core/core_test.go
@@ -3,3 +3,51 @@
 // license that can be found in the LICENSE file.
 
 package core
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/z0rr0/luss/auth"
+	"github.com/z0rr0/luss/conf"
+	"github.com/z0rr0/luss/db"
+	"github.com/z0rr0/luss/test"
+	"golang.org/x/net/context"
+)
+
+func TestHandlerTest(t *testing.T) {
+	cfg, err := conf.Parse(test.TcConfigName())
+	if err != nil {
+		t.Fatalf("invalid behavior")
+	}
+	err = cfg.Validate()
+	if err != nil {
+		t.Fatalf("invalid behavior")
+	}
+	ctx, cancel := context.WithCancel(conf.NewContext(cfg))
+	defer cancel()
+
+	r := &http.Request{}
+	ctx, _ = auth.CheckToken(ctx, r, false)
+
+	s, err := db.NewSession(cfg.Conn, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer s.Close()
+	ctx = db.NewContext(ctx, s)
+
+	ctx, err = auth.Authenticate(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	if err := HandlerTest(ctx, w, r); err.Err != nil {
+		t.Error(err.Err)
+	}
+	if w.Code != http.StatusOK {
+		t.Error("invalid behavior")
+	}
+}
